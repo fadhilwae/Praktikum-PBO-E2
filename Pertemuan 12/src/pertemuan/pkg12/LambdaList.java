37d2ebac049ccde4d9a3cd519bef4dc59b1f133c
fadhilwae <fadhilyaafiwidodo7@gmail.com>
2026-06-19 20:07:45 +0700

    Tentang Lambda

---
+++ Pertemuan 12/src/pertemuan/pkg12/LambdaList.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pertemuan.pkg12;
+
+/**
+ *
+ * @author Fadhil Yaafi W
+ */
+import java.util.ArrayList;
+
+public class LambdaList {
+    public static void main(String[] args) {
+        ArrayList<String> mahasiswaList = new ArrayList<>();
+        
+        mahasiswaList.add("Adi");
+        mahasiswaList.add("Bambang");
+        mahasiswaList.add("Cici");
+        mahasiswaList.add("Didi");
+        
+        // lambda digunakan sebagai parameter
+        mahasiswaList.forEach((nama) -> System.out.println(nama));
+    }
+}
